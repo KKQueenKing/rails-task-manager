e4af1e2d099ea2218291408813bc182d35eead59
Katie King <katharinemargaretking@gmail.com>
2020-02-18 15:10:46 +1100

    understanding

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,34 @@
 class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # @restaurant = Restaurant.new(params[:restaurant])
+    # @restaurant.save
+    redirect_to task_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
 end
